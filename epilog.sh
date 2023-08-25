80fa46c649f6bc867f0aae2ba96f865479a1099f
Michael Robbert <mrobbert@mines.edu>
2023-08-25 14:37:41 -0600

    Use SLURM_JOB_ACCOUNT for projects field

--- epilog.sh
+++ epilog.sh
@@ -3,10 +3,4 @@
 export SLURM_ROOT=/sw/apps/slurm/active
 sed -i "0,/${SLURM_JOB_USER}/d" /tmp/jobs/jobs_users
 sed -i "0,/${SLURM_JOBID}/d" /tmp/jobs/jobs_ids
-
-#load the comment of the job.
-Project=$($SLURM_ROOT/bin/scontrol show job ${SLURM_JOB_ID} | grep Comment | awk -F'=' '{print $2}')
-Project_Tag=""
-if [ ! -z "${Project}" ];then
-  sed -i "0,/${Project}/d" /tmp/jobs/jobs_projects
-fi
+sed -i "0,/${SLURM_JOB_ACCOUNT}/d" /tmp/jobs/jobs_projects
--- prolog.sh
+++ prolog.sh
@@ -4,10 +4,4 @@
 export SLURM_ROOT=/sw/apps/slurm/active
 echo "${SLURM_JOB_USER}" >> /tmp/jobs/jobs_users
 echo "${SLURM_JOBID}" >> /tmp/jobs/jobs_ids
-
-#load the comment of the job.
-Project=$($SLURM_ROOT/bin/scontrol show job ${SLURM_JOB_ID} | grep Comment | awk -F'=' '{print $2}')
-Project_Tag=""
-if [ ! -z "${Project}" ];then
-  echo "${Project}" >> /tmp/jobs/jobs_projects
-fi
+echo "${SLURM_JOB_ACCOUNT}" >> /tmp/jobs/jobs_projects
